e08ccc6d2bd5b691368a4ebe31013f7b51d9ee1c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-16 20:16:13 +0545

    feat: add next-auth buttons and env

---
+++ apps/anilist-ani-mate/app/api/auth/[…nextauth]/route.ts
@@ -0,0 +1,23 @@
+// import { authOptions } from '@/lib/auth';
+// import NextAuth from 'next-auth';
+//
+// const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST };
+
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: 'process.env.GOOGLE_ID!',
+      clientSecret: 'process.env.GOOGLE_SECRET!',
+    }),
+    // ...add more providers here
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
