48b7b8ed692120397c8ef40c75ea11175b94442c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-16 16:21:56 +0545

    refactor: navbar to feature/ui-lib

--- apps/anilist-ani-mate/app/components/index.tsx
+++ apps/anilist-ani-mate/app/components/index.tsx
@@ -6,15 +6,7 @@ import { Avatar, AvatarGroup, AvatarIcon } from '@nextui-org/react';
 export function Hello() {
   return (
     <div className="bg-indigo-500 p-2 font-mono">
-      <Ui/>
-      <div className="flex gap-3 items-center">
-        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-        <Avatar name="Junior" />
-        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-        <Avatar name="Jane" />
-        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
-        <Avatar name="Joe" />
-      </div>
+      <Ui />
     </div>
   );
 }
