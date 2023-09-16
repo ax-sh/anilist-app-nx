1d07ffeed8c90b7cb4c1529ca95ad9f8e6dda861
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-16 20:18:38 +0545

    feat: add auth lib

---
+++ auth/src/lib/auth.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import Auth from './auth';
+
+describe('Auth', () => {
+  
+  it('should render successfully', () => {
+    const { baseElement } = render(<Auth />);
+    expect(baseElement).toBeTruthy();
+  });
+  
+});
