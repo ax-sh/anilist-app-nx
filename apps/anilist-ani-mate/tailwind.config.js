ea5c2c0d2bd43035dbcc52b49b90f9c2b7eef198
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-16 15:17:22 +0545

    feat: add next ui and examples

--- apps/anilist-ani-mate/tailwind.config.js
+++ apps/anilist-ani-mate/tailwind.config.js
@@ -2,7 +2,6 @@ import { createGlobPatternsForDependencies } from '@nx/react/tailwind';
 import { join } from 'path';
 import { nextui } from '@nextui-org/react';
 
-console.log(__dirname, 99);
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,7 +10,11 @@ module.exports = {
       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
     ),
     ...createGlobPatternsForDependencies(__dirname),
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    join(
+      __dirname,
+      '../../',
+      './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    ),
   ],
   theme: {
     extend: {},
