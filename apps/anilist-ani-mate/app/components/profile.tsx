ee8d401cb8cfa58b81527dbe32bb972105641889
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-17 02:32:35 +0545

    refactor: readability

--- apps/anilist-ani-mate/app/components/profile.tsx
+++ apps/anilist-ani-mate/app/components/profile.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { useQuery } from '@apollo/client';
-import userProfile from '../graphql/user-profile.gql';
+import { userProfileQuerySchema } from '@anilist-app-nx/auth';
 
 export function Profile() {
   // const o = client.query(userProfile);
 
-  const { data, loading } = useQuery(userProfile);
-  if (loading) return <>Loading</>;
+  const { data, loading, error } = useQuery(userProfileQuerySchema);
+  if (error) return <pre>error {JSON.stringify(error, null, 4)}</pre>;
+  if (loading) return <pre>Loading</pre>;
   return <div>{JSON.stringify(data)}</div>;
 }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/anilist-provider';
 export * from './lib/auth-buttons';
+export * as userProfileQuerySchema from './lib/graphql/user-profile.gql';
