b69c72f8a38841d25ad9aa17fcf198f1b50ef01e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-17 02:54:48 +0545

    feat: add get headers util

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,3 +1,11 @@
 export * from './lib/anilist-provider';
 export * from './lib/auth-buttons';
-export * as userProfileQuerySchema from './lib/graphql/user-profile.gql';
+export * as ANILIST_USER_PROFILE_QUERY from './lib/graphql/user-profile.gql';
+
+export function getHeaders(accessToken: string | undefined) {
+  return {
+    Authorization: 'Bearer ' + accessToken,
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
+}
