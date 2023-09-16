d77df0897b3c2467007ffef02b43c061d13446fa
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-17 02:54:53 +0545

    feat: add get headers util

--- apps/anilist-ani-mate/app/components/profile.tsx
+++ apps/anilist-ani-mate/app/components/profile.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import { useQuery } from '@apollo/client';
-import { userProfileQuerySchema } from '@anilist-app-nx/auth';
+import { ANILIST_USER_PROFILE_QUERY } from '@anilist-app-nx/auth';
 
 export function Profile() {
   // const o = client.query(userProfile);
 
-  const { data, loading, error } = useQuery(userProfileQuerySchema);
+  const { data, loading, error } = useQuery(ANILIST_USER_PROFILE_QUERY);
   if (error) return <pre>error {JSON.stringify(error, null, 4)}</pre>;
   if (loading) return <pre>Loading</pre>;
   return <div>{JSON.stringify(data)}</div>;
