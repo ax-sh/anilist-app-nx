4dd8f90a8e126c833eb1f36128ff244c05794af1
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-16 16:24:59 +0545

    refactor: navbar to feature/ui-lib

--- apps/anilist-ani-mate/app/components/index.tsx
+++ apps/anilist-ani-mate/app/components/index.tsx
@@ -1,7 +1,5 @@
 'use client';
 import { Ui } from '@anilist-app-nx/ui';
-// import { Avatar } from '@nextui-org/react';
-import { Avatar, AvatarGroup, AvatarIcon } from '@nextui-org/react';
 
 export function Hello() {
   return (
