1466fcbe94f5be122411bb2c6acb85ad67763196
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-23 13:36:25 +0545

    feat: add codegen.yml

--- apps/anilist-ani-mate/codegen.ts
+++ apps/anilist-ani-mate/codegen.ts
@@ -1,13 +1,13 @@
-import { CodegenConfig } from '@graphql-codegen/cli';
+// import { CodegenConfig } from '@graphql-codegen/cli'
 
-console.log(777);
+type CodegenConfig = any;
 const config: CodegenConfig = {
   schema: 'https://anilist.co/graphiql',
   documents: [
     '/Users/axmin/Desktop/PROJECT/anilist-app-nx/auth/src/lib/graphql/**/*.gql',
   ],
   generates: {
-    './src/gql/': {
+    './': {
       preset: 'client',
     },
   },
