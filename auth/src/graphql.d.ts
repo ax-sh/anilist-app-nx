8ec325ce1a89edffe66d544b03e787ae68816a3b
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-23 15:55:42 +0545

    feat: add eslint for graphql files

--- auth/src/graphql.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.gql' {
-  // TODO add codegen for auto type casting graphql files
-  import { DocumentNode } from 'graphql';
-
-  const value: DocumentNode;
-  export default value;
-}
