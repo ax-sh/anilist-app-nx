80f3f2af3fc6e2f7a8b588ca3876c2b727a29b93
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-10-04 05:40:33 +0545

    feat(anilist-ani-mate): add anime-card.tsx

---
+++ apps/anilist-ani-mate/app/anime/anime-card.tsx
@@ -0,0 +1,30 @@
+import { Badge, Card, CardFooter } from '@nextui-org/react';
+import Image from 'next/image';
+
+export function AnimeCard({ src, name }: { src: string; name: string }) {
+  return (
+    <Card isFooterBlurred radius="lg" className="border-none">
+      <Image
+        alt="img"
+        className="object-cover"
+        height={200}
+        width={200}
+        src={src}
+      />
+      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1  py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_1rem)] shadow-small z-10">
+        <Badge content={''}>
+          <p className="text-lg text-black/80 font-bold">{name}</p>
+        </Badge>
+        {/*<Button*/}
+        {/*  className="text-tiny text-white bg-black/20"*/}
+        {/*  variant="flat"*/}
+        {/*  color="default"*/}
+        {/*  radius="lg"*/}
+        {/*  size="sm"*/}
+        {/*>*/}
+        {/*  Notify me*/}
+        {/*</Button>*/}
+      </CardFooter>
+    </Card>
+  );
+}
--- apps/anilist-ani-mate/app/anime/characters-section.tsx
+++ apps/anilist-ani-mate/app/anime/characters-section.tsx
@@ -1,36 +1,10 @@
-import { Avatar, Badge, Button, Card, CardFooter } from '@nextui-org/react';
-import Image from 'next/image';
-export default function CharaCard({ src }: { src: string }) {
-  return (
-    <Card isFooterBlurred radius="lg" className="border-none">
-      <Image
-        alt="img"
-        className="object-cover"
-        height={200}
-        width={200}
-        src={src}
-      />
-      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">Available soon.</p>
-        <Button
-          className="text-tiny text-white bg-black/20"
-          variant="flat"
-          color="default"
-          radius="lg"
-          size="sm"
-        >
-          Notify me
-        </Button>
-      </CardFooter>
-    </Card>
-  );
-}
+import { Avatar, Badge } from '@nextui-org/react';
+import { AnimeCard } from './anime-card';
 
 export function CharactersSection({ characters }: { characters: any[] }) {
   return (
     <section className={'flex gap-2 flex-wrap'}>
       {characters.map((character) => {
-        character.gender;
         return (
           <div key={character.id}>
             <Badge content={character.favourites} color={'primary'}>
@@ -50,7 +24,8 @@ export function CharactersSection({ characters }: { characters: any[] }) {
           </div>
         );
       })}
-      <CharaCard
+      <AnimeCard
+        name={'Reisalin Stout'}
         src={
           'https://s4.anilist.co/file/anilistcdn/character/large/b223350-yNmgtin88wwu.png'
         }
