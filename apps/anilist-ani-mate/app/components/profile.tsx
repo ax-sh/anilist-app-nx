b18241162b81bc751276df368dc50735e1b876ea
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-10-04 04:48:32 +0545

    feat(profile): Add error display functionality in Profile component (develop)

--- apps/anilist-ani-mate/app/components/profile.tsx
+++ apps/anilist-ani-mate/app/components/profile.tsx
@@ -1,17 +1,19 @@
 'use client';
 
-import { useQuery } from '@apollo/client';
+import { ApolloError, useQuery } from '@apollo/client';
 import { ANILIST_USER_PROFILE_QUERY } from '@anilist-app-nx/auth';
+
+function ShowErrorJson({ error }: { error: ApolloError }) {
+  return (
+    <pre>
+      {error.graphQLErrors.map((error) => JSON.stringify(error, null, 4))}
+    </pre>
+  );
+}
 
 export function Profile() {
   const { data, loading, error } = useQuery(ANILIST_USER_PROFILE_QUERY);
-  if (error)
-    return (
-      <pre>
-        error{' '}
-        {error.graphQLErrors.map((error) => JSON.stringify(error, null, 4))}
-      </pre>
-    );
+  if (error) return <ShowErrorJson error={error} />;
   if (loading) return <pre>Loading</pre>;
-  return <div>{JSON.stringify(data)}</div>;
+  return <pre>{JSON.stringify(data, null, 4)}</pre>;
 }
