fe2f9b6e953ffe1edcccd5e683520056df85ee94
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-02 22:12:36 +0545

    feat(linting): update eslint configuration

--- apps/anilist-ani-mate/app/anime/anime-card.tsx
+++ apps/anilist-ani-mate/app/anime/anime-card.tsx
@@ -1,15 +1,15 @@
 import { Badge, Card, CardFooter } from '@nextui-org/react';
 import Image from 'next/image';
 
-export function AnimeCard({ name, src }: { src: string; name: string }) {
+export function AnimeCard({ name, src }: { name: string; src: string }) {
   return (
     <Card isFooterBlurred radius="lg" className="border-none">
       <Image
         alt="img"
+        src={src}
         className="object-cover"
         height={200}
         width={200}
-        src={src}
       />
       <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1  py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_1rem)] shadow-small z-10">
         <Badge content={''}>
