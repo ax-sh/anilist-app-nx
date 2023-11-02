65d1a0964ff7b4002b650d458982bad751f7c5b9
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-02 19:07:29 +0545

    feat(eslint): add eslint-plugin-write-good-comments (feature/eslint-custom-config)
    - Added eslint-plugin-write-good-comments to package.json dependencies.
    - Added 'write-good-comments/write-good-comments' rule to .eslintrc.ax-sh.js.
    - Updated pnpm-lock.yaml with eslint-plugin-write-good-comments version.
    - Added eslint-plugin-write-good-comments to .eslintrc.ax-sh.js plugins.
    - Updated pnpm-lock.yaml with eslint-plugin-write-good-comments version.
    - Added eslint-plugin-write-good-comments to .eslintrc.ax-sh.js plugins.
    - Updated pnpm-lock.yaml with eslint-plugin-write-good-comments version.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added

--- apps/anilist-ani-mate/mocks/handlers.ts
+++ apps/anilist-ani-mate/mocks/handlers.ts
@@ -1,5 +1,8 @@
-import { http, HttpResponse } from 'msw';
+import { graphql, http, HttpResponse } from 'msw';
 
 export const handlers = [
   http.get('/resource', () => HttpResponse.json({ id: 'abc-123' })),
+  graphql.query('ListPosts', ({ query }) => {
+    console.log('Intercepted a "ListPosts" GraphQL query:', query);
+  }),
 ];
