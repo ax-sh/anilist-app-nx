d9bbada91075ccff24c8639f524507ba90a1d023
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-02 19:16:50 +0545

    feature/msw graphql (#4)
    * feat(eslint): add eslint-plugin-write-good-comments (feature/eslint-custom-config)
    
    - Added eslint-plugin-write-good-comments to package.json dependencies.
    - Added 'write-good-comments/write-good-comments' rule to .eslintrc.ax-sh.js.
    - Updated pnpm-lock.yaml with eslint-plugin-write-good-comments version.
    - Added eslint-plugin-write-good-comments to .eslintrc.ax-sh.js plugins.
    - Updated pnpm-lock.yaml with eslint-plugin-write-good-comments version.
    - Added eslint-plugin-write-good-comments to .eslintrc.ax-sh.js plugins.
    - Updated pnpm-lock.yaml with eslint-plugin-write-good-comments version.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added eslint-plugin-write-good-comments package to pnpm-lock.yaml.
    - Added
    
    * feat: Add Apollo GraphQL queries to page and home-container components
    
    - Import gql and useQuery from @apollo/client
    - Add a GraphQL query to retrieve a list of posts
    - Console log the data from the query
    
    Issue: feature/msw-graphql (msw-graphql)

--- apps/anilist-ani-mate/mocks/handlers.ts
+++ apps/anilist-ani-mate/mocks/handlers.ts
@@ -1,5 +1,16 @@
-import { http, HttpResponse } from 'msw';
+import { graphql, http, HttpResponse } from 'msw';
 
+const anilist = graphql.link('https://graphql.anilist.co');
 export const handlers = [
   http.get('/resource', () => HttpResponse.json({ id: 'abc-123' })),
+  anilist.query('ListPosts', ({ query }) => {
+    console.log('Intercepted a "ListPosts" GraphQL query:', query);
+    return HttpResponse.json({
+      data: {
+        // Convert all posts to an array
+        // and return as the "posts" root-level property.
+        posts: { k: 9 },
+      },
+    });
+  }),
 ];
