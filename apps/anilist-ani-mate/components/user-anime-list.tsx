85c67c84fbaa82afaad49cda05572c39c41f5bc9
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-12 18:29:02 +0545

    feat: update schema

--- apps/anilist-ani-mate/components/user-anime-list.tsx
+++ apps/anilist-ani-mate/components/user-anime-list.tsx
@@ -1,31 +1,27 @@
-/* eslint-disable-next-line */
 import {
-  AnimePartsFragment,
-  Maybe,
-  Media,
-  MediaListCollection,
-  MediaListGroup,
-  UserAnimeListQuery,
+  IMediaListGroup,
   useUserAnimeListQuery,
 } from '../generated/graphql/graphql';
-import { AnimeParts } from '*/*.gqlanime.gql';
 
 export interface UserAnimeListProps {}
 
-function AnimeList({ data }: { data: MediaListGroup[] }) {
+function AnimeList({ data }: { data: IMediaListGroup[] }) {
   console.log(data, 33);
-  return 'dfdf';
+  return <>anime list</>;
 }
 
 export function UserAnimeList(props: UserAnimeListProps) {
   const { data, error, loading } = useUserAnimeListQuery({
     variables: { username: '' },
   });
-  if (loading) return 'loading';
+  if (loading) return <>loading</>;
+  if (error) return <>{error.message}</>;
 
   return (
     <div>
-      <AnimeList data={data!.MediaListCollection!.lists! as MediaListGroup[]} />
+      <AnimeList
+        data={data!.MediaListCollection!.lists! as IMediaListGroup[]}
+      />
     </div>
   );
 }
