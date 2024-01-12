d286fc8ef6e46b770595a6285b672af2847b27da
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-12 13:17:16 +0545

    feat: update schema

--- apps/anilist-ani-mate/mocks/mock-data/userAnimeListMockData.ts
+++ apps/anilist-ani-mate/mocks/mock-data/userAnimeListMockData.ts
@@ -1,28 +1,28 @@
+const media = {
+  media: {
+    id: 130592,
+    title: {
+      romaji: 'Haaaaaataraku Maou-sama!!',
+      __typename: 'MediaTitle',
+    },
+    coverImage: {
+      extraLarge:
+        'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx130592-LAUlhx15mxQu.jpg',
+      __typename: 'MediaCoverImage',
+    },
+    idMal: 48413,
+    status: 'FINISHED',
+    __typename: 'Media',
+  },
+  __typename: 'MediaList',
+};
+
 export const UserAnimeListMockData = {
   data: {
     MediaListCollection: {
       lists: [
         {
-          entries: [
-            {
-              media: {
-                id: 130592,
-                title: {
-                  romaji: 'Haaaaaataraku Maou-sama!!',
-                  __typename: 'MediaTitle',
-                },
-                coverImage: {
-                  extraLarge:
-                    'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx130592-LAUlhx15mxQu.jpg',
-                  __typename: 'MediaCoverImage',
-                },
-                idMal: 48413,
-                status: 'FINISHED',
-                __typename: 'Media',
-              },
-              __typename: 'MediaList',
-            },
-          ],
+          entries: [media],
           __typename: 'MediaListGroup',
         },
       ],
