f046e910cbba2f5a01695fc05d393fea036e89ca
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-12 19:48:49 +0545

    feat: add transformUserAnimeList

--- apps/anilist-ani-mate/components/user-anime-list.tsx
+++ apps/anilist-ani-mate/components/user-anime-list.tsx
@@ -11,7 +11,7 @@ function AnimeList({ results }: { results: IAnimePartsFragment[] }) {
     <section>
       {results.map((result) => {
         return (
-          <div key={result.id} className={'aspect-square'}>
+          <div key={result.id} className={'aspect-square overflow-hidden h-40'}>
             <img alt="series" src={result.coverImage?.extraLarge} />
           </div>
         );
