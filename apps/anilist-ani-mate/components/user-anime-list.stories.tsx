8381cc7678b9b67e02a1ca82169691f53adffb03
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-12 13:24:02 +0545

    feat: update schema

--- apps/anilist-ani-mate/components/user-anime-list.stories.tsx
+++ apps/anilist-ani-mate/components/user-anime-list.stories.tsx
@@ -6,6 +6,7 @@ import { expect } from '@storybook/jest';
 import { graphql, HttpResponse } from 'msw';
 import { UserAnimeListMockData } from '../mocks/mock-data/userAnimeListMockData';
 import { AniMateProvider } from '../app/providers';
+import { MockedProvider } from '@apollo/client/testing'; // Use for Apollo Version 3+
 
 const meta: Meta<typeof UserAnimeList> = {
   component: UserAnimeList,
@@ -38,6 +39,10 @@ Heading.decorators = [
 ];
 
 Heading.parameters = {
+  apolloClient: {
+    MockedProvider,
+    // any props you want to pass to MockedProvider on every story
+  },
   msw: {
     handlers: [
       anilist.query('UserAnimeList', ({ query }) =>
