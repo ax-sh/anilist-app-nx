34f08e8151a917ef72016211930637aa83d3f7bf
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-16 19:35:28 +0545

    feat: add on click popover

--- apps/anilist-ani-mate/mocks/mock-data/userAnimeListMockData.ts
+++ apps/anilist-ani-mate/mocks/mock-data/userAnimeListMockData.ts
@@ -1,30 +1,32 @@
 import { faker } from '@faker-js/faker';
 
-const media = {
-  media: {
-    id: 130592,
-    title: {
-      romaji: 'Haaaaaataraku Maou-sama!!',
-      __typename: 'MediaTitle',
+function genMedia() {
+  return {
+    media: {
+      id: 130592,
+      title: {
+        romaji: faker.company.name(),
+        __typename: 'MediaTitle',
+      },
+      coverImage: {
+        extraLarge:
+          'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx130592-LAUlhx15mxQu.jpg',
+        __typename: 'MediaCoverImage',
+      },
+      idMal: 48413,
+      status: 'FINISHED',
+      __typename: 'Media',
     },
-    coverImage: {
-      extraLarge:
-        'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx130592-LAUlhx15mxQu.jpg',
-      __typename: 'MediaCoverImage',
-    },
-    idMal: 48413,
-    status: 'FINISHED',
-    __typename: 'Media',
-  },
-  __typename: 'MediaList',
-};
+    __typename: 'MediaList',
+  };
+}
 
 export const UserAnimeListMockData = {
   data: {
     MediaListCollection: {
       lists: [
         {
-          entries: faker.helpers.multiple(() => media),
+          entries: faker.helpers.multiple(genMedia, { count: 10 }),
           __typename: 'MediaListGroup',
         },
       ],
