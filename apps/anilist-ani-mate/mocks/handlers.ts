17303b9f0a0bc2b16d3df665d18244ccb3a00cff
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-16 17:04:14 +0545

    fix: apollo provider on storybook

--- apps/anilist-ani-mate/mocks/handlers.ts
+++ apps/anilist-ani-mate/mocks/handlers.ts
@@ -4,16 +4,6 @@ import { anilistLink } from '../app/constants';
 
 export const handlers = [
   // http.get('/resource', () => HttpResponse.json({ id: 'abc-123' })),
-  anilistLink.query('ListPosts', ({ query }) => {
-    console.log('Intercepted a "ListPosts" GraphQL query:', query);
-    return HttpResponse.json({
-      data: {
-        // Convert all posts to an array
-        // and return as the "posts" root-level property.
-        posts: { k: 9 },
-      },
-    });
-  }),
   anilistLink.query('UserAnimeList', ({ query }) =>
     HttpResponse.json(UserAnimeListMockData, { status: 201 }),
   ),
