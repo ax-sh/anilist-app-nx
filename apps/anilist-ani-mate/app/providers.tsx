049fb0821cab83eb9b13a5007754f1044508b951
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-16 16:27:33 +0545

    feat: remove linting rule

--- apps/anilist-ani-mate/app/providers.tsx
+++ apps/anilist-ani-mate/app/providers.tsx
@@ -3,7 +3,7 @@
 import { NextUIProvider } from '@nextui-org/react';
 import { SessionProvider } from 'next-auth/react';
 
-import { PropsWithChildren, useMemo } from 'react';
+import { type PropsWithChildren } from 'react';
 
 import { ApolloProviderWrapper } from './apollo-provider-wrapper';
 
