c599d8550a4f40615c14b05cde13edf8c060c9b0
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-17 17:22:20 +0545

    fix: eslint error

--- apps/anilist-ani-mate/components/anime-card-container.tsx
+++ apps/anilist-ani-mate/components/anime-card-container.tsx
@@ -8,7 +8,7 @@ export function AnimeCardContainer<T extends Record<string, unknown>>(
   return (
     <section className={'grid grid-cols-4 flex-wrap container gap-2'}>
       {props.rows.map((row, index) => (
-        <props.renderRow {...row} key={index} />
+        <props.renderRow key={index} {...row} />
       ))}
     </section>
   );
