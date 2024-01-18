f674012d824a060728f72eb09b7eae8a545ce12b
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-18 21:44:36 +0545

    fix: eslint error

--- apps/anilist-ani-mate/features/characters/utils/transform-anime-characters.tsx
+++ apps/anilist-ani-mate/features/characters/utils/transform-anime-characters.tsx
@@ -9,7 +9,7 @@ export function transformAnimeCharacters(data: IAnimeQuery) {
   const results = data.Media;
   const characters = results?.characters
     ?.nodes as DeepRequired<ICharacterPartsFragment>[];
-  if (!characters) return [];
+  if (!Array.isArray(characters)) return [];
   const sorted = [...characters];
   sorted.sort(characterSortPredicate);
   return sorted;
